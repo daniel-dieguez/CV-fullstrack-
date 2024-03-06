7da5e9ae242640c15ab66eae17cace4f51d8869f
daniel-dieguez <danguez2001@gmail.com^C>
2024-03-06 16:55:34 -0600

    Se realizaron varios cambios, junto a ello la validacion del formulario y ademas la integracion de imagenes de lenguajes.

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from '../styles/inicio.module.css'//./inicio.module.css'
+import fotoperfil from '../img/Foto pefil.jpg'
 
 export default function Inicio() {
 
@@ -16,14 +17,13 @@ export default function Inicio() {
       <h3>Un ambiente de desarrollo profecional </h3>
       <h4>Daniel Dieguez</h4>
 
-      <img src="" alt="" srcset="" />
+      <img src={fotoperfil} alt="" srcset="" />
 
-
-        
-      
-      
-      
-      
+      <p>En este proyecto, destaco la importancia de tener metas y objetivos claros como piedra 
+        angular para alcanzar el éxito. Más allá de depender únicamente de la motivación, pongo 
+        énfasis en la consistencia diaria en la ejecución de tareas. Explora mi portafolio para no 
+        solo conocer mis habilidades técnicas en desarrollo frontend y backend, sino también para 
+        entender mi enfoque hacia el logro sostenible de metas profesionales.</p>   
       
       
       
