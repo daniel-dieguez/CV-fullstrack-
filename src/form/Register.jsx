974ca5386e214d57ced394e455c3222d1acd83ac
daniel-dieguez <danguez2001@gmail.com^C>
2024-06-05 12:50:31 -0600

    Realice cambios, ahora ya se puede enviar los datos en el post, ademas de realizar la correcion para que se muestren los datos del get

--- src/form/Register.jsx
+++ src/form/Register.jsx
@@ -1,21 +1,59 @@
 import React, { useState } from 'react'
 import { Form, Button} from 'react-bootstrap';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { apiPost } from '../api/ApiPost';
+
 
 export default function Register( {errores, onSubmitCallback}) {
 
 
-    const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
   const [text, setText] = useState("");
+  const [nombre, setnombre] =useState("");
+
+  const [dato, setDato] = useState([]);
 
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    onSubmitCallback({ email, text }); /*Aqui se realiza la evaluacion de los datos*/
-  }
+
+ const submitForm = (e) => {
+  e.preventDefault();
+  onSubmitCallback({email, text, nombre});
+
+  const data = {
+    nombre_usuario: nombre,
+    email_usuario: email,
+    comentario_usuario: text
+};
+
+  const URL = "http://localhost:9000/registro/v1/cv/registro";
+  fetch(URL,{
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' },
+
+  }).then((r) => {
+    return r.json().then((data) =>({
+      status: r.status,
+      body:data,
+    }));
+  })
+
+
+  
+}
+
+
+  
 
 
   return (
     <Form onSubmit={submitForm}>
+      <Form.Group className="mb-2">
+      <Form.Label> Ingrese su nombre y apellido</Form.Label>
+      <Form.Control type='name' value={nombre} onChange={e => setnombre(e.target.value)} placeholder='Nombre y apellido' isInvalid={errores.nombre}></Form.Control>
+      </Form.Group>
+
     <Form.Group className="mb-2">
       <Form.Label> Correo Electronico</Form.Label>
       <Form.Control type='email' value={email} onChange={e => setEmail(e.target.value)} placeholder='Correo' isInvalid={errores.email}></Form.Control>
